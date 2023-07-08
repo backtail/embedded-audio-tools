15e6c137f19cbc0068362d54eb1d32ca22132ab9
backtail <accounts@maxgenson.de>
2023-07-08 14:28:04 +0200

    add samples to time utility traits for floats

--- src/float/mod.rs
+++ src/float/mod.rs
@@ -1,5 +1,6 @@
 mod bit_manipulation;
 mod conversion;
+mod dsp_util;
 mod interpolation;
 mod math;
 
@@ -7,6 +8,7 @@ pub(crate) mod integral;
 
 pub use bit_manipulation::*;
 pub use conversion::*;
+pub use dsp_util::DSPUtility;
 pub use interpolation::*;
 pub use math::AdditionalF32Ext;
 pub use micromath::F32Ext;
